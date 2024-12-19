e8f0c92fa9aadf2c22710b51dcd58ba5785df3d6
fengdang168 <fengdang168@gmail.com>
2024-12-19 12:14:50 +0800

    Update 双击换壁纸.js

--- assets/双击换壁纸.js
+++ assets/双击换壁纸.js
@@ -1,8 +1,8 @@
 var bodyBgs =[
          "https://hefollo.com/apis.php?type=本人使用壁纸",
-       "https://hefollo.com/apis.php?type=电脑端-植物图片",
-	   "https://hefollo.com/apis.php?type=电脑端-美女图片",
-	   "https://hefollo.com/apis.php?type==电脑端-谷歌每日图片",
+         "https://hefollo.com/apis.php?type=电脑端-植物图片",
+	 "https://hefollo.com/apis.php?type=电脑端-美女图片",
+	"https://hefollo.com/apis.php?type=电脑端-谷歌每日图片",
 	  	         ];  //API接口
 function clock(){
 var randomBgIndex = Math.floor( Math.random() * 4+1);
